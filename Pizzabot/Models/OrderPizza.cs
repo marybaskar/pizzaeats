9e872a708b8d0f5336886ca90143280e231764ee
mary <mbarbaraskar_6603@email.ric.edu>
2019-02-25 13:18:38 -0500

    Chatbot Application

---
+++ Pizzabot/Models/OrderPizza.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Bot.Builder.FormFlow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pizzabot.Models
+{
+    public enum PizzaSizeOptions
+    {
+        small,
+        medium,
+        large,
+    }
+
+    public enum PizzaTopping
+    {
+      Pepperoni,
+      Mushrooms,
+      Onions,
+      Sausage,
+      Bacon,
+      cheese,
+      Olives,
+      Peppers,
+      Pineapples,
+      Spinach,
+       
+    }
+
+    [Serializable]
+    public class OrderPizza
+    {
+        public PizzaSizeOptions? PizzaSize;
+        public DateTime? OrderDate;
+        public List<PizzaTopping> Topping;
+
+        public static IForm<OrderPizza> BuildForm()
+        {
+            return new FormBuilder<OrderPizza>()
+              .Message("welcome to pizzaeats bot!")
+              .Build();
+        }    
+    }
+}

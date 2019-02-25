890682be3ff61f6b406f1fe9bb18961505f72416
Alexis Polonsky <jaeseo21@gmail.com>
2019-02-25 13:06:56 -0500

    Front-End Initial Re-Upload

---
+++ home.php
@@ -0,0 +1,18 @@
+
+<?php
+  require "chatbox.php";
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> PizzaEats</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+
+    <div class="container-pastorders">
+      <p> Past Orders </p>
+    </div>
+
+  </body>
+</html>

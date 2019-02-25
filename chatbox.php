ba4c5f69b76ae3bed29d2ff667d7ffed534deb06
Alexis Polonsky <jaeseo21@gmail.com>
2019-02-25 12:42:27 -0500

    Front End Initial Upload

---
+++ chatbox.php
@@ -0,0 +1,41 @@
+<div class="chatbox">
+
+  <div class="chatlogs">
+
+      <div class="chat bot">
+        <div class="user-photo"><img src="img/pizza-icon.png"></div>
+        <p class="chat-message">How can I help you</p>
+      </div>
+      <div class="chat user">
+        <div class="user-photo"><img src="img/g-icon.png"></div>
+        <p class="chat-message">I want a pizza</p>
+      </div>
+      <div class="chat bot">
+        <div class="user-photo"><img src="img/pizza-icon.png"></div>
+        <p class="chat-message">What kind of pizza would you like?</p>
+      </div>
+      <div class="chat user">
+        <div class="user-photo"><img src="img/g-icon.png"></div>
+        <p class="chat-message">The Pizza kind</p>
+      </div>
+      <div class="chat bot">
+        <div class="user-photo"><img src="img/pizza-icon.png"></div>
+        <p class="chat-message">What kind of pizza would you like?</p>
+      </div>
+      <div class="chat user">
+        <div class="user-photo"><img src="img/g-icon.png"></div>
+        <p class="chat-message">What do you have?</p>
+      </div>
+      <div class="chat bot">
+        <div class="user-photo"><img src="img/pizza-icon.png"></div>
+        <p class="chat-message">We have Pepperoni, Barbeque, Cheese, and Hawaiian Pizza</p>
+      </div>
+
+    </div>
+
+    <div class="chat-form">
+      <textarea></textarea>
+      <button>Send</button>
+    </div>
+
+  </div>

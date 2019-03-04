67fde7a02d098881a666e404d2d96ec2fc1e092c
Alexis Polonsky <jaeseo21@gmail.com>
2019-03-03 19:02:16 -0500

    update to match the new index

--- Front-End Development/header.php
+++ Front-End Development/header.php
@@ -8,12 +8,7 @@
 
     <header>
       <nav class="nav-header-main">
-        <a class="header-logo" href="home.php">
+        <a class="header-logo" href="index.html">
         <img src="img/pizza-logo.png">
         </a>
-        <ul>
-          <li><a href="index.php">Home</a></li>
-          <li><a href="Sign-in.php">Sign-in</a></li>
-          <li><a href="checkout.php">Checkout</a></li>
-        </ul>
       </nav>

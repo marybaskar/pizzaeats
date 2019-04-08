400b0503e3c092b1042df4741dfc220354d152b9
Alexis Polonsky <jaeseo21@gmail.com>
2019-04-08 12:03:07 -0400

    PizzaEats without Azure

---
+++ LUIS-pizzaorder/LUIS-pizzaorder/LUIS-pizzaorder/Models/LUIS.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace JSONUtils
+{
+
+    public class TopScoringIntent
+    {
+        public string intent { get; set; }
+        public double score { get; set; }
+    }
+
+    public class Intent
+    {
+        public string intent { get; set; }
+        public double score { get; set; }
+    }
+
+    public class Entity
+    {
+        public string entity { get; set; }
+        public string type { get; set; }
+        public int startIndex { get; set; }
+        public int endIndex { get; set; }
+        public double score { get; set; }
+    }
+
+    public class LUIS
+    {
+        public string query { get; set; }
+        public TopScoringIntent topScoringIntent { get; set; }
+        public IList<Intent> Intents { get; set; }
+        public IList<Entity> Entities { get; set; }
+    }
+
+    public class Query
+    {
+        public string Size { get; set; }
+        public string Topping { get; set; }
+        public string Number { get; set; }
+        //public string Greeting { get; set; }
+    }
+
+}

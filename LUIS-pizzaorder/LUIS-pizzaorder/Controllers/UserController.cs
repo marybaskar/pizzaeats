9002c603bbbcc9775d36744dec00a32b068baa8e
berriosmarisela <47118557+berriosmarisela@users.noreply.github.com>
2019-04-18 15:05:58 -0400

    Revert "Revert "login progress""
    This reverts commit 96360a39716a00453a76be4283749a98a8875d7d.

---
+++ LUIS-pizzaorder/LUIS-pizzaorder/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LUIS_pizzaorder.Models;
+
+namespace LUIS_pizzaorder.Controllers
+{
+    public class UserController : Controller
+    {
+        //Register
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration([Bind(Exclude = "user_id")] user newUser)
+        {
+            bool Status = false;
+            string Message = "";
+
+            if(ModelState.IsValid)
+            {
+
+            }
+            else
+            {
+                Message = "Invalid Request";
+            }
+            return View(newUser);
+        }
+        [NonAction]
+        public bool usernameExists(string username)
+        {
+            using (
+        }
+
+        //Login
+
+        //LogOut
+    }
+}

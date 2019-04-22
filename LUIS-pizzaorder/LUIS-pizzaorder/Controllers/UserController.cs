4fb3c8e41e107d1f80ee3b75f809126eb95a2769
Alexis Polonsky <jaeseo21@gmail.com>
2019-04-22 12:39:19 -0400

    Seeing if this allows me to pull

--- LUIS-pizzaorder/LUIS-pizzaorder/Controllers/UserController.cs
+++ LUIS-pizzaorder/LUIS-pizzaorder/Controllers/UserController.cs
@@ -24,72 +24,19 @@ namespace LUIS_pizzaorder.Controllers
 
             if(ModelState.IsValid)
             {
-                bool isExist = UsernameExists(newUser.username);
-                if(isExist)
-                {
-                    ModelState.AddModelError("UsernameExist", "Username already exists");
-                    return View(newUser);
-
-                }
-
-                newUser.password = Crypto.Hash(newUser.password);
-                newUser.comfirmpassword = Crypto.Hash(newUser.comfirmpassword);
-
-                using (UserContext dc = new UserContext())
-                {
-                    dc.users.Add(newUser);
-                    dc.SaveChanges();
-                }
 
             }
             else
             {
                 Message = "Invalid Request";
             }
-            ViewBag.Status = Status;
             return View(newUser);
         }
-
-        [HttpGet]
-        public ActionResult Login()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Login(UserLogin login)
-        {
-            string Message = "";
-            using (UserContext dc = new UserContext())
-            {
-                var v = dc.users.Where(a => a.username == login.username).FirstOrDefault();
-                if(v!=null)
-                {
-                    if(string.Compare(Crypto.Hash(login.password), v.password)==0)
-                    {
-
-                    }
-                    else
-                    {
-                        Message = "Incorrect Password";
-                    }
-                }
-                else { Message = "Username doesnot exist"; }
-            }
-
-            ViewBag.Message = Message;
-            return View();
-        }
-
-       [NonAction]
-       public bool UsernameExists(string username)
-       {
-            using (UserContext dc = new UserContext())
-            {
-                var v = dc.users.Where(a => a.username == username).FirstOrDefault();
-                return v != null;
-            }
-       }
+       // [NonAction]
+       // public bool usernameExists(string username)
+       // {
+       //     using (
+       // }
 
         //Login
 
--- LUIS-pizzaorder/LUIS-pizzaorder/Models/Extended/user.cs
+++ LUIS-pizzaorder/LUIS-pizzaorder/Models/Extended/user.cs
@@ -18,42 +18,34 @@ namespace LUIS_pizzaorder.Models
 
 
         public int user_id { get; set; }
-        [Display(Name = "Username")]
+
         [Required(AllowEmptyStrings = false, ErrorMessage = errorString)]
         public string username { get; set; }
 
-        [Display(Name = "Password")]
         [Required(AllowEmptyStrings = false, ErrorMessage = errorString)]
         [DataType(DataType.Password)]
-        [StringLength(60, MinimumLength = 6)]
+        [MinLength(6,ErrorMessage = "Minimum 6 characters required")]
         public string password { get; set; }
 
-        [Display(Name = "Confirm Password")]
         [DataType(DataType.Password)]
-        [Compare("password", ErrorMessage = "Confirm password and password do not match")]
+        [Compare("password",ErrorMessage ="Confirm password ad password do not match")]
         public string comfirmpassword { get; set; }
 
-        [Display(Name = "First Name")]
         [Required(AllowEmptyStrings = false, ErrorMessage = errorString)]
         public string firstname { get; set; }
 
-        [Display(Name = "Last Name")]
         [Required(AllowEmptyStrings = false, ErrorMessage = errorString)]
         public string lastname { get; set; }
 
-        [Display(Name = "Address")]
         [Required(AllowEmptyStrings = false, ErrorMessage = errorString)]
         public string address { get; set; }
 
-        [Display(Name = "Phone Number")]
         [Required(AllowEmptyStrings = false, ErrorMessage = errorString)]
         [DataType(DataType.PhoneNumber)]
         public string phone { get; set; }
 
-        [Display(Name = "Email Address")]
         [Required(AllowEmptyStrings = false, ErrorMessage = errorString)]
         [DataType(DataType.EmailAddress)]
-        [StringLength(60, MinimumLength = 6)]
         public string email { get; set; }
     }
 }

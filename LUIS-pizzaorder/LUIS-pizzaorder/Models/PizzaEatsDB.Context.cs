5b071f245676585758786a26b20e485102602338
berriosmarisela <47118557+berriosmarisela@users.noreply.github.com>
2019-04-22 12:17:35 -0400

    Registration works

--- LUIS-pizzaorder/LUIS-pizzaorder/Models/PizzaEatsDB.Context.cs
+++ LUIS-pizzaorder/LUIS-pizzaorder/Models/PizzaEatsDB.Context.cs
@@ -13,10 +13,10 @@ namespace LUIS_pizzaorder.Models
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     
-    public partial class Entities : DbContext
+    public partial class pizzadbEntities : DbContext
     {
-        public Entities()
-            : base("name=Entities")
+        public pizzadbEntities()
+            : base("name=pizzadbEntities")
         {
         }
     
